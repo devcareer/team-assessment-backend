c75e95ad977aa008a164e26726f5479731a09e12
Olaitan Adedokun <kenney9224@gmail.com>
2023-01-19 22:40:59 +0100

    Create the route, router and controller template

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const userRoute = require("./routers/userRoute");
 
 const app = express();
 
 app.use(express());
+
+app.use('/api/v1/users', userRoute);
 
 app.all('*', (req, res) => {
     res.status(404).send(`The route ${req.originalUrl} does not exist!`);
